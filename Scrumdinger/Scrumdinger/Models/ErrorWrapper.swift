44981571ddb2fe3aed6d5b9eb9c07f8ccbff79db
Andre Cocuroci <andremartins@luizalabs.com>
2022-12-19 16:21:21 -0300

    Handling errors

---
+++ Scrumdinger/Scrumdinger/Models/ErrorWrapper.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorWrapper.swift
+//  Scrumdinger
+//
+//  Created by Andre on 19/12/22.
+//
+
+import Foundation
+
+struct ErrorWrapper: Identifiable {
+    let id: UUID
+    let error: Error
+    let guidance: String
+
+    init(id: UUID = UUID(), error: Error, guidance: String) {
+        self.id = id
+        self.error = error
+        self.guidance = guidance
+    }
+}
