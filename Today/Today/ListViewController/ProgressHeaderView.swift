85765a9eeb585a3e04157d83ddb65c88eedaf8ae
Andre Cocuroci <andremartins@luizalabs.com>
2023-02-13 10:30:47 -0300

    Creating a progress view

---
+++ Today/Today/ListViewController/ProgressHeaderView.swift
@@ -0,0 +1,97 @@
+//
+//  ProgressHeaderView.swift
+//  Today
+//
+//  Created by Andre on 13/02/23.
+//
+
+import UIKit
+
+final class ProgressHeaderView: UICollectionReusableView {
+    static var elementKind: String { UICollectionView.elementKindSectionHeader }
+
+    var progress: CGFloat = 0 {
+        didSet {
+            setNeedsLayout()
+            heightConstraint?.constant = progress * bounds.height
+            UIView.animate(withDuration: 0.2) { [weak self] in
+                self?.layoutIfNeeded()
+            }
+        }
+    }
+
+    private var heightConstraint: NSLayoutConstraint?
+    private var valueFormat: String {
+        NSLocalizedString("%d percent", comment: "progress percentage value format")
+    }
+
+    private let upperView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let lowerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    private let containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        prepareSubviews()
+        isAccessibilityElement = true
+        accessibilityLabel = NSLocalizedString("Progress", comment: "Progress view accessibility label")
+        accessibilityTraits.update(with: .updatesFrequently)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        accessibilityValue = String(format: valueFormat, Int(progress * 100.0))
+        heightConstraint?.constant = progress * bounds.height
+        containerView.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 0.5 * containerView.bounds.width
+    }
+
+    private func prepareSubviews() {
+        containerView.addSubview(upperView)
+        containerView.addSubview(lowerView)
+        addSubview(containerView)
+
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1),
+            containerView.heightAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 1),
+
+            containerView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            containerView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            containerView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85),
+
+            upperView.topAnchor.constraint(equalTo: topAnchor),
+            upperView.bottomAnchor.constraint(equalTo: lowerView.topAnchor),
+            lowerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            upperView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            upperView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            lowerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            lowerView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+
+        heightConstraint = lowerView.heightAnchor.constraint(equalToConstant: 0)
+        heightConstraint?.isActive = true
+
+        backgroundColor = .clear
+        containerView.backgroundColor = .clear
+        upperView.backgroundColor  = .todayProgressUpperBackground
+        lowerView.backgroundColor  = .todayProgressLowerBackground
+    }
+}
--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -20,14 +20,35 @@ final class ReminderListViewController: UICollectionViewController {
 
     var listStyleSegmentedControl = UISegmentedControl(items: ReminderListStyle.allCases.map { $0.name })
 
+    var headerView: ProgressHeaderView?
+    var progress: CGFloat {
+        let chunkSize = 1.0 / CGFloat(filteredReminders.count)
+        let progress = filteredReminders.reduce(0.0) {
+            let chunk = $1.isComplete ? chunkSize : 0
+            return $0 + chunk
+        }
+        return progress
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         collectionView.collectionViewLayout = listLayout()
+        collectionView.backgroundColor = .todayGradientFutureBegin
 
         let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
 
         dataSource = DataSource(collectionView: collectionView) { (collectionView, indexPath, itemIdentifier: Reminder.ID) in
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+
+        let headerRegistration = UICollectionView.SupplementaryRegistration(
+            elementKind: ProgressHeaderView.elementKind,
+            handler: supplementaryRegistrationHandler
+        )
+
+        dataSource.supplementaryViewProvider = { view, elementKind, indexPath in
+            self.collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
         }
 
         let addButton = UIBarButtonItem(
@@ -73,8 +94,22 @@ final class ReminderListViewController: UICollectionViewController {
         navigationController?.pushViewController(detailViewController, animated: true)
     }
 
+    override func collectionView(
+        _ collectionView: UICollectionView,
+        willDisplaySupplementaryView view: UICollectionReusableView,
+        forElementKind elementKind: String,
+        at indexPath: IndexPath
+    ) {
+        guard elementKind == ProgressHeaderView.elementKind, let progressHeaderView = view as? ProgressHeaderView else {
+            return
+        }
+
+        progressHeaderView.progress = progress
+    }
+
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.headerMode = .supplementary
         listConfiguration.showsSeparators = false
         listConfiguration.backgroundColor = .clear
         listConfiguration.trailingSwipeActionsConfigurationProvider = makeSwipeActions
@@ -94,5 +129,13 @@ final class ReminderListViewController: UICollectionViewController {
         }
 
         return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+
+    private func supplementaryRegistrationHandler(
+        progressView: ProgressHeaderView,
+        elementKind: String,
+        indexPath: IndexPath
+    ) {
+        headerView = progressView
     }
 }
